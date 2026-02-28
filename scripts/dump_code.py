96ff2a3156452991ceb69ddec266b06b280922de
Ignacio Jara <ignacio.jara.o@ug.uchile.cl>
2026-02-28 19:49:32 -0300

    Remove all tracking except notebooks and dash, excluding csv data

--- scripts/dump_code.py
+++
@@ -1,9 +0,0 @@
-import json
-
-with open("notebooks/SpaceX-Machine-Learning-Prediction-Part-5-v1.ipynb", "r") as f:
-    nb = json.load(f)
-
-with open("scripts/notebook_code.py", "w") as out:
-    for cell in nb['cells']:
-        if cell['cell_type'] == 'code':
-            out.write("".join(cell['source']) + "\n\n")
